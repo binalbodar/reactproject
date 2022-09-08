a01765c91992930d3c572328104ea53d127428d8
binalbodar <binalbodar23@gmail.com>
2022-09-07 19:25:16 -0700

    7/9/22

--- src/Componet/Header/Header.js
+++ src/Componet/Header/Header.js
@@ -2,91 +2,60 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header(props) {
-    return (
-        <div>
-  <title>Minishop - Free Bootstrap 4 Template by Colorlib</title>
-  <meta charSet="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet" />
-  <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
-  <link rel="stylesheet" href="css/animate.css" />
-  <link rel="stylesheet" href="css/owl.carousel.min.css" />
-  <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-  <link rel="stylesheet" href="css/magnific-popup.css" />
-  <link rel="stylesheet" href="css/aos.css" />
-  <link rel="stylesheet" href="css/ionicons.min.css" />
-  <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
-  <link rel="stylesheet" href="css/jquery.timepicker.css" />
-  <link rel="stylesheet" href="css/flaticon.css" />
-  <link rel="stylesheet" href="css/icomoon.css" />
-  <link rel="stylesheet" href="css/style.css" />
-  <div className="py-1 bg-black">
-    <div className="container">
-      <div className="row no-gutters d-flex align-items-start align-items-center px-md-0">
-        <div className="col-lg-12 d-block">
-          <div className="row d-flex">
-            <div className="col-md pr-4 d-flex topper align-items-center">
-              <div className="icon mr-2 d-flex justify-content-center align-items-center"><span className="icon-phone2" /></div>
-              <span className="text">+ 1235 2355 98</span>
-            </div>
-            <div className="col-md pr-4 d-flex topper align-items-center">
-              <div className="icon mr-2 d-flex justify-content-center align-items-center"><span className="icon-paper-plane" /></div>
-              <span className="text">youremail@email.com</span>
-            </div>
-            <div className="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
-              <span className="text">3-5 Business days delivery &amp; Free Returns</span>
+  return (
+    <>
+      <div className="py-1 bg-black">
+        <div className="container">
+          <div className="row no-gutters d-flex align-items-start align-items-center px-md-0">
+            <div className="col-lg-12 d-block">
+              <div className="row d-flex">
+                <div className="col-md pr-4 d-flex topper align-items-center">
+                  <div className="icon mr-2 d-flex justify-content-center align-items-center"><span className="icon-phone2" /></div>
+                  <span className="text">+ 1235 2355 98</span>
+                </div>
+                <div className="col-md pr-4 d-flex topper align-items-center">
+                  <div className="icon mr-2 d-flex justify-content-center align-items-center"><span className="icon-paper-plane" /></div>
+                  <span className="text">youremail@email.com</span>
+                </div>
+                <div className="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
+                  <span className="text">3-5 Business days delivery &amp; Free Returns</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div className="container">
-      <a className="navbar-brand" href="index.html">Minishop</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="oi oi-menu" /> Menu
-      </button>
-      <div className="collapse navbar-collapse" id="ftco-nav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            {/* <a href="index.html" className="nav-link">Home</a> */}
-            <NavLink exact to={"/"} className="nav-link">Home</NavLink>
-        </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catalog</a>
-            <div className="dropdown-menu" aria-labelledby="dropdown04">
-              {/* <a className="dropdown-item" href="shop.html">Shop</a> */}
-              <NavLink exact className="dropdown-item" to={"/shop"}>Shop</NavLink>
-              {/* <a className="dropdown-item" href="product-single.html">Single Product</a> */}
-              <NavLink exact className="dropdown-item" to={"/singalproduct"}>Single Product</NavLink>
-              {/* <a className="dropdown-item" href="cart.html">Cart</a> */}
-              <NavLink exact className="dropdown-item" to={"/cart"}>Cart</NavLink>
-              {/* <a className="dropdown-item" href="checkout.html">Checkout</a> */}
-              <NavLink exact className="dropdown-item" to={"/checkout"}>Checkout</NavLink>
-            </div>
-          </li>
-          <li className="nav-item">
-            {/* <a href="about.html" className="nav-link">About</a> */}
-            <NavLink exact to={"/about"} className="nav-link">About</NavLink>
-          </li>
-          <li className="nav-item">
-            {/* <a href="blog.html" className="nav-link">Blog</a> */}
-            <NavLink exact to={"/blog"} className="nav-link">Blog</NavLink>
-          </li>
-          <li className="nav-item">
-            {/* <a href="contact.html" className="nav-link">Contact</a> */}
-            <NavLink exact to={"/contact"} className="nav-link">Contact</NavLink>
-          </li>
-          <li className="nav-item cta cta-colored"><a href="cart.html" className="nav-link"><span className="icon-shopping_cart" />[0]</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-</div>
-
-
-    );
+      <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">Minishop</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="oi oi-menu" /> Menu
+          </button>
+          <div className="collapse navbar-collapse" id="ftco-nav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                {/* <a href="index.html" className="nav-link">Home</a> */}
+                <NavLink exact to={"/"} className="nav-link">Home</NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catalog</a>
+                <div className="dropdown-menu" aria-labelledby="dropdown04">
+                  <a className="dropdown-item" href="shop.html">Shop</a>
+                  <a className="dropdown-item" href="product-single.html">Single Product</a>
+                  <a className="dropdown-item" href="cart.html">Cart</a>
+                  <a className="dropdown-item" href="checkout.html">Checkout</a>
+                </div>
+              </li>
+              <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
+              <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
+              <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
+              <li className="nav-item cta cta-colored"><a href="cart.html" className="nav-link"><span className="icon-shopping_cart" />[0]</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
