e09262d6ed6a7b76ffa5baa314322ca95d5e094d
deepvaghani <vaghanideep31@gmail.com>
2022-10-05 08:15:15 +0530

    5/10/22

--- src/Componet/Addtocart/Addtocart.js
+++ src/Componet/Addtocart/Addtocart.js
@@ -1,31 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { gettoCart } from '../../Redux/Action/Cart.action';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Addtocart(props) {
+function Addtocart(props) {  
+    const Filterdata = [];
+
+    const Product = useSelector(state => state.Product)
     const Cart = useSelector(state => state.Cart)
-    const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(gettoCart())
-    }, [])
+    Cart.Cart.map(C => Product.Product.map((P)=>{
+        if(C.id === P.id){
+            const data = {
+                ...P,
+                qunty: C.qunty
+            }
+            Filterdata.push(data)
+            
+        }
+    }))
+    console.log(Filterdata);
+    
     return (
         <div>
             {
-                Cart.Cart.map((a) => {
-                    return (
-                        <section>
-                            <div className="d-flex row">
-                                <NavLink to={{
-                                    pathname: '/categoryview',
-                                    state: { id: a.id }
-                                }}>
-                                    <img src={a.url} alt="" className="d-flex rounded img-fluid border category-imag" width="180px" />
-                                </NavLink>
-                                <h4 className='text-center mt-3 mb-3'>{a.name}</h4>
-                            </div>
-                        </section>
+                Filterdata.map((C)=>{
+                    return(
+                        <tr>
+                            {/* <td><img src={C.url} alt="" width={100}></td> */}
+                            <td>{C.name}</td>
+                            <td>{C.price}</td>
+                        </tr>
                     )
                 })
             }
--- src/Redux/Action/Cart.action.js
+++ src/Redux/Action/Cart.action.js
@@ -1,11 +1,5 @@
 import * as ActionTypes from "../ActionType"
 
-export const gettoCart = () => (dispatch) => {
-    dispatch({
-        type: ActionTypes.GET_CART
-    })
-}
-
 export const addtoCart = (d) => (dispatch) => {
     dispatch({
         type: ActionTypes.ADD_CART, payload: { id: d.id, qunty: 1 }
